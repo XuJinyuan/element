407a9291fbabdc7043c055e8402dcc8ec49987e5
Jason Xu(许金源) <xujy@inspur.com>
2022-07-12 11:20:04 +0800

    base

--- build/release.sh
+++ build/release.sh
@@ -16,10 +16,10 @@ then
   VERSION=$VERSION npm run dist
 
   # ssr test
-  node test/ssr/require.test.js            
+  node test/ssr/require.test.js
 
   # publish theme
-  echo "Releasing theme-chalk $VERSION ..."
+  echo "Releasing theme-base $VERSION ..."
   cd packages/theme-chalk
   npm version $VERSION --message "[release] $VERSION"
   if [[ $VERSION =~ "beta" ]]
@@ -35,12 +35,12 @@ then
   git commit -m "[build] $VERSION"
   npm version $VERSION --message "[release] $VERSION"
 
-  # publish
-  git push eleme master
-  git push eleme refs/tags/v$VERSION
-  git checkout dev
-  git rebase master
-  git push eleme dev
+#  # publish
+#  git push eleme master
+#  git push eleme refs/tags/v$VERSION
+#  git checkout dev
+#  git rebase master
+#  git push eleme dev
 
   if [[ $VERSION =~ "beta" ]]
   then
